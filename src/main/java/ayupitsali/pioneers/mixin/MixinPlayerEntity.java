53962827f7bcfb80ef588bb65ecc88f9c7c15043
Ali <ali.dee1410@gmail.com>
2024-05-09 22:01:06 +0100

    Player display name formatting

--- src/main/java/ayupitsali/pioneers/mixin/MixinPlayerEntity.java
+++ src/main/java/ayupitsali/pioneers/mixin/MixinPlayerEntity.java
@@ -6,16 +6,21 @@ import ayupitsali.pioneers.data.PioneerData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
+    @Shadow protected abstract MutableText addTellClickEvent(MutableText component);
+
     protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -43,5 +48,12 @@ public abstract class MixinPlayerEntity extends LivingEntity {
             playerData.addLives(-1);
             sendMessage(Text.translatable("lives.lives_changed.death", playerData.getLivesDisplay()));
         }
+    }
+
+    @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
+    private void onGetDisplayName(CallbackInfoReturnable<Text> cir) {
+        LivesGroup playerGroup = ModComponents.PIONEER_DATA.get(this).getLivesGroup();
+        MutableText displayName = getName().copy().formatted(playerGroup.getColourFormatting());
+        cir.setReturnValue(addTellClickEvent(displayName));
     }
 }
